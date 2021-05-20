fff5b1529087eadedf8bc9c19d9aaa6e4d3598b0
N V S Aditya <aditya.nadamuni@gmail.com>
2021-05-21 00:51:46 +0530

    Newer design

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const newTodo = document.querySelector('.add');
 const list = document.querySelector('.todos');
 const del = document.querySelector('.delete');
+const dateDisplay = document.querySelector('.date');
+
 const generateTodoTemplate = (data)=>{
     const liTemplate = `<li class="list-group-item text-light d-flex justify-content-between align-items-center">
             <span>${data}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <i class="fas fa-minus-circle"></i>
         </li>`;
     return liTemplate;
 }
@@ -34,3 +36,18 @@ list.addEventListener('click', (e)=>{
         e.target.parentElement.remove()
     }
 })
+
+
+let objToday = new Date(),
+    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+    dayOfWeek = weekday[objToday.getDay()],
+    domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+    dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+    curMonth = months[objToday.getMonth()],
+    curYear = objToday.getFullYear()
+
+// var today = dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+var today = `Its ${dayOfWeek} ${dayOfMonth} of ${curMonth}, ${curYear}`;
+
+dateDisplay.textContent = today
