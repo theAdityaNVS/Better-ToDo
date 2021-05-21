0c44cb536ab3caeee24168f87e3fdd9c9aa8ca07
N V S Aditya <aditya.nadamuni@gmail.com>
2021-05-21 20:24:48 +0530

    replaced date format

--- app.js
+++ app.js
@@ -38,18 +38,13 @@ list.addEventListener('click', (e)=>{
     }
 })
 
+let now = new Date();
+let dayOfWeek = dateFns.format(now, 'dddd');
+let dayOfMonth = dateFns.format(now, 'Do');
+let curMonth = dateFns.format(now, 'MMMM');
+let curYear = dateFns.format(now, 'GGGG');
 
-let objToday = new Date(),
-    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-    dayOfWeek = weekday[objToday.getDay()],
-    domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-    dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-    curMonth = months[objToday.getMonth()],
-    curYear = objToday.getFullYear()
-
-// var today = dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
-var today = `Its ${dayOfWeek} ${dayOfMonth} of ${curMonth}, ${curYear}`;
+let today = `Its ${dayOfWeek} ${dayOfMonth} of ${curMonth}, ${curYear}`;
 
 dateDisplay.textContent = today
 
